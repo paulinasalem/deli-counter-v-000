8ad20e06fc059ef834c5f0ef6386a751fe816017
Paulina Salem <paulinasalem@yahoo.com>
2018-04-20 19:00:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+def line(array)
+if array.length == 0
+  puts "The line is currently empty."
+else
+  new_variable=""
+    array.each_with_index do |people_online, index|
+      new_variable<<" #{index+1}. #{people_online}"
+end
+puts "The line is currently:#{new_variable}"
+end
+end
+
+def take_a_number
+
+end
